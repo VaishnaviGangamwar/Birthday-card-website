338b72bf8ec8e3bd77990df2dd98987bb44a7483
VaishnaviGangamwar <vaishnavigangamwar2001@gmail.com>
2023-12-20 00:00:43 +0530

    add file

---
+++ src/Data.js
@@ -0,0 +1,26 @@
+const Data=[
+    {
+        'name':"Shahrukh Khan",
+        'age' : 57,
+        'image': "https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
+    }
+    ,
+    {
+        'name':"Akshay Kumar",
+        'age' : 45,
+        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKW-E2ANTMDwWeGQxb8TdD8cVryY2aMMbCRqYqy-x3JkW-oz0O"
+    }
+    ,
+    {
+        'name':"Virat Kohli",
+        'age' : 37,
+        'image': "https://sambadenglish.com/wp-content/uploads/2022/11/Virat-Kohli-1.jpg"
+    }
+    ,
+    {
+        'name':"Deepika Padukone",
+        'age' : 32,
+        'image': "https://assets.vogue.in/photos/64be31695ad7ce31037005c8/3:4/w_2560%2Cc_limit/Snapinsta.app_362218118_18390206827011278_5161897771700955906_n_1080.jpg"
+    }
+]
+export default Data
